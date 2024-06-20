0d5e136ab0ff5b6f9ea53b3a8096a51a9225e1ab
JyojiTenguam <j.y.oji@hotmail.com>
2024-06-20 16:54:43 -0300

    Resolvendo requisito 01 de Project talker manager

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises; // Importa o módulo fs para trabalhar com promessas
 
 const app = express();
 app.use(express.json());
@@ -11,7 +12,23 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// Endpoint GET /talker
+app.get('/talker', async (_request, response) => {
+  try {
+    const talkers = await fs.readFile('src/talker.json', 'utf-8');
+    const talkerslist = JSON.parse(talkers);
+
+    if (talkerslist.length > 0) {
+      response.status(HTTP_OK_STATUS).send(talkerslist);
+    } else {
+      response.status(HTTP_OK_STATUS).send([]);
+    }
+  } catch (error) {
+    // Em caso de erro na leitura do arquivo, retorna um array vazio
+    response.status(HTTP_OK_STATUS).send([]);
+  }
+});
+
 app.listen(PORT, () => {
   console.log('Online');
-  console.log('hello world');
 });
