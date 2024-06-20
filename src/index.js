1fd7c00b754df31078e46901623c823624922245
JyojiTenguam <j.y.oji@hotmail.com>
2024-06-20 18:31:54 -0300

    Resolvendo requisito 04 de Project talker manager

--- src/index.js
+++ src/index.js
@@ -50,7 +50,25 @@ app.get('/talker/:id', async (request, response) => {
   }
 });
 
-app.post('/login', (request, response) => {
+function validateLogin(request, response, next) {
+  const { email, password } = request.body;
+  if (!email) {
+    return response.status(400).send({ message: 'O campo "email" é obrigatório' });
+  }
+  const emailRegex = /\S+@\S+\.\S+/;
+  if (!emailRegex.test(email)) {
+    return response.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return response.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return response.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+app.post('/login', validateLogin, (request, response) => {
   // Gera um token aleatório de 16 caracteres
   const token = crypto.randomBytes(8).toString('hex');
   response.status(200).send({ token });
